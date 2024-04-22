dfd1142ffc974041e245021e8be947aba02cc8bf
Kexin Ding <111871149+Ding-Kexin@users.noreply.github.com>
2024-04-22 12:51:42 +0800

    Update LGG_CNN.py

--- Model/LGG_CNN.py
+++ Model/LGG_CNN.py
@@ -73,10 +73,11 @@ class GlobalLocalGatedConvNet(nn.Module):
             nn.BatchNorm2d(512),
             nn.ReLU()
         )
-
-        # self.local_gated_module1 = Gated_Conv(128, 128, ksize=3)
-        # self.local_gated_module2 = Gated_Conv(256, 256, ksize=3)
-        # self.local_gated_module3 = Gated_Conv(512, 512, ksize=3)
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU()
+        )
 
         self.local_gated_conv1 = nn.Sequential(
             nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
@@ -91,60 +92,16 @@ class GlobalLocalGatedConvNet(nn.Module):
             nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
         )
 
-        # self.global_gated_module1 = GlobalGatedModule(patch_size=patch_size)
-        # self.global_gated_module2 = GlobalGatedModule(patch_size=int(patch_size/2))
-        # self.global_gated_module3 = GlobalGatedModule(patch_size=int(patch_size/4))
-
-        # self.gaijinECA1 = gaijinECA_Module(patch_size=31)
-        # self.gaijinECA2 = gaijinECA_Module(patch_size=15)
-        # self.gaijinECA3 = gaijinECA_Module(patch_size=7)
-
         self.fusion1 = nn.Sequential(
-            # nn.Conv2d(128, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2),
-            # nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(256),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2),
-            # nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2),
-            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.AdaptiveAvgPool2d(1)
             nn.Linear(128 * 2, self.classes),
         )
 
         self.fusion2 = nn.Sequential(
             nn.Linear(256 * 2, self.classes),
-            # nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(256),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2),
-            # nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2),
-            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.AdaptiveAvgPool2d(1)
         )
 
         self.fusion3 = nn.Sequential(
-            nn.Linear(512 , self.classes),
-            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2),
-            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            # nn.AdaptiveAvgPool2d(1)
+            nn.Linear(512 * 2, self.classes),
         )
 
         self.fusion4 = nn.Sequential(
@@ -166,9 +123,6 @@ class GlobalLocalGatedConvNet(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(),
         )
-        # self.global_gate43 = GlobalGatedModule2(in_patch_size=3, out_patch_size=7)
-        # self.global_gate32 = GlobalGatedModule2(in_patch_size=7, out_patch_size=15)
-        # self.global_gate21 = GlobalGatedModule2(in_patch_size=15, out_patch_size=31)
 
         self.global_gate43 = GlobalGatedModule2(in_patch_size=int(patch_size/8), out_patch_size=int(patch_size/4))
         self.global_gate32 = GlobalGatedModule2(in_patch_size=int(patch_size/4), out_patch_size=int(patch_size/2))
@@ -177,10 +131,9 @@ class GlobalLocalGatedConvNet(nn.Module):
         self.global_pooling = nn.AdaptiveAvgPool2d(1)
         self.finally_fc_classification = nn.Linear(512 * 4, self.classes)
 
-        self.coefficient1 = torch.nn.Parameter(torch.Tensor([0.25]))
-        self.coefficient2 = torch.nn.Parameter(torch.Tensor([0.25]))
-        self.coefficient3 = torch.nn.Parameter(torch.Tensor([0.25]))
-        self.coefficient4 = torch.nn.Parameter(torch.Tensor([0.25]))
+        self.coefficient1 = torch.nn.Parameter(torch.Tensor([0.33]))
+        self.coefficient2 = torch.nn.Parameter(torch.Tensor([0.33]))
+        self.coefficient3 = torch.nn.Parameter(torch.Tensor([0.34]))
 
     def forward(self, patchX, pixelX):  # x:(64,103,31,31)
 
@@ -188,80 +141,33 @@ class GlobalLocalGatedConvNet(nn.Module):
         x_input = patchX
         x1 = self.conv1(x_input)
         x2 = self.conv2(x1)
-        # local_map1 = self.local_gated_module1(x2)
-        # local_map1 = self.gaijinECA1(x2)
-        # global_map1 = self.global_gated_module1(x2)
-        # out1 = self.fusion1(torch.cat((local_map1, global_map1), dim=1))
         local_gate1 = F.softmax(self.local_gated_conv1(x2), dim=1)
-
         x3 = nn.MaxPool2d(2)(x2)
         x3 = self.conv3(x3)
         x4 = self.conv4(x3)
-
         conv2to1 = self.conv2to1(x4)
         global_gate2to1 = self.global_gate21(conv2to1)
         out1 = nn.AdaptiveAvgPool2d(1)(torch.cat((local_gate1 * x2, global_gate2to1 * x2), dim=1))
-        # out1 = nn.AdaptiveAvgPool2d(1)(local_gate1 * x2)
         out1 = out1.view(out1.size(0), -1)
         out1 = self.fusion1(out1)
         out1 = F.softmax(out1, dim=1)
-
-        # local_map2 = self.local_gated_module2(x4)
-        # local_map2 = self.gaijinECA2(x4)
-        # global_map2 = self.global_gated_module2(x4)
-        # out2 = self.fusion2(torch.cat((local_map2, global_map2), dim=1))
-        # out2 = self.fusion2(local_map2)
-        # out2 = out2.view(out2.size(0), -1)
         local_gate2 = F.softmax(self.local_gated_conv2(x4), dim=1)
-        # global_gate2 = self.global_gated_module2(x4)
-        # out2 = nn.AdaptiveAvgPool2d(1)(torch.cat((local_gate2, global_gate2), dim=1))
-        # out2 = out2.view(out2.size(0), -1)
-        # out2 = self.fusion2(out2)
-        # out2 = F.softmax(out2, dim=1)
-        # conv2to1 = self.conv2to1(x4)
-        # global_gate2to1 = self.global_gate21(conv2to1)
-        # out1 = nn.AdaptiveAvgPool2d(1)(torch.cat((local_gate1 * x2, global_gate2to1 * x2), dim=1))
-        # out1 = out1.view(out1.size(0), -1)
-        # out1 = self.fusion1(out1)
-        # out1 = F.softmax(out1, dim=1)
-
         x5 = nn.MaxPool2d(2)(x4)
         x5 = self.conv5(x5)
         x6 = self.conv6(x5)
-        # local_map3 = self.local_gated_module3(x6)
-        # local_map3 = self.gaijinECA3(x6)
-        # global_map3 = self.global_gated_module3(x6)
-        # out3 = self.fusion3(torch.cat((local_map3, global_map3), dim=1))
-        # out3 = self.fusion3(local_map3)
-        # out3 = out3.view(out3.size(0), -1)
         conv3to2 = self.conv3to2(x6)
         global_gate3to2 = self.global_gate32(conv3to2)
         out2 = nn.AdaptiveAvgPool2d(1)(torch.cat((local_gate2 * x4, global_gate3to2 * x4), dim=1))
-        # out2 = nn.AdaptiveAvgPool2d(1)(local_gate2 * x4)
         out2 = out2.view(out2.size(0), -1)
         out2 = self.fusion2(out2)
         out2 = F.softmax(out2, dim=1)
-
-        local_gate3 = F.softmax(self.local_gated_conv3(x6), dim=1)
-
         x7 = nn.MaxPool2d(2)(x6)
         x7 = self.conv7(x7)
-
-        # local_gate4 = F.softmax(self.local_gated_conv4(x7), dim=1)
-        # out4 = nn.AdaptiveAvgPool2d(1)(x7 * local_gate4)
-        # conv4to3 = self.conv4to3(x7)
-        # global_gate4to3 = self.global_gate43(conv4to3)
-        # out3 = nn.AdaptiveAvgPool2d(1)(torch.cat((local_gate3 * x6, global_gate4to3 * x6), dim=1))
-        out3 = nn.AdaptiveAvgPool2d(1)(local_gate3 * x6)
-        out3 = out3.view(out3.size(0), -1)
-        out3 = self.fusion3(out3)
-        out3 = F.softmax(out3, dim=1)
-
         out4 = nn.AdaptiveAvgPool2d(1)(x7)
         out4 = out4.view(out4.size(0), -1)
         out4 = self.fusion4(out4)
         out4 = F.softmax(out4, dim=1)
 
-        output = self.coefficient1 * out1 + self.coefficient2 * out2 + self.coefficient3 * out3 + self.coefficient4 * out4
+        output = self.coefficient1 * out1 + self.coefficient2 * out2 + self.coefficient3 * out4
 
-        return output, output
+        return output, output, self.coefficient1, self.coefficient2, self.coefficient3
